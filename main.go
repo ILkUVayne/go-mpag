6b95d87662c063514bfe13a10e0b5f5128e71482
luoyang <1193055746@qq.com>
2023-09-05 18:45:43 +0800

    add: 实现视频添加文字水印

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
-	"log"
-	"os"
-	"os/exec"
+	"fmt"
+	"go-mpeg/linuxExec/watermark"
 )
 
+// ffmpeg -hide_banner -i /mnt/f/video/1251203951-1-192.mp4 -i /mnt/f/video/watermark.jpg -filter_complex "overlay=x=0:y=0" out.mp4 -y
+// ffmpeg -i /mnt/f/video/1251203951-1-192.mp4 -vf "drawtext=fontsize=100:fontfile=lazy.ttf:text='hello world':x=20:y=20:fontcolor=#123fff@.5:box=1:boxcolor=yellow" out1.mp4
+// ffmpeg -i /mnt/f/video/1251203951-1-192.mp4 -vf "drawtext=fontsize=100:fontfile=lazy.ttf:textfile=./1.text:x=20:y=20:fontcolor=#123fff:box=1:boxcolor=yellow" out1.mp4
+
+// 简单filtergraphs 视频和音频分别-vf和-af
 func main() {
-	c := exec.Command("ffmpeg", "-h")
-	c.Stdout = os.Stdout
-	c.Stderr = os.Stderr
-	err := c.Run()
+	srcPath := "/mnt/f/video/1251203951-1-192.mp4"
+	dstPath := "/mnt/f/video/out3.mp4"
+	dt := watermark.NewDrawText(
+		watermark.WithText("happy!"),
+		watermark.WithTransparency(0.3),
+		watermark.WithFontsize(50),
+	)
+	err := dt.Watermark(srcPath, dstPath)
 	if err != nil {
-		log.Fatalf("failed to call cmd.Run(): %v", err)
+		fmt.Printf("%s\n", err.Error())
 	}
 }
