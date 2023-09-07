701f1b9dbe87b74341d59052c4c5da721bdaa4ab
luoyang <1193055746@qq.com>
2023-09-07 16:55:47 +0800

    test user

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import "go-mpeg/linuxExec/watermark"
 
 // ffmpeg -i /mnt/f/video/1251203951-1-192.mp4 -i /mnt/f/video/watermark.jpg -i /mnt/f/video/watermark.jpg -i /mnt/f/video/watermark.jpg -i /mnt/f/video/watermark.jpg -filter_complex "[1:v]scale=160:90,format=yuva444p,colorchannelmixer=aa=0.4[img1];[2:v]scale=160:90,format=yuva444p,colorchannelmixer=aa=0.4[img2];[3:v]scale=160:90,format=yuva444p,colorchannelmixer=aa=0.4[img3];[4:v]scale=160:90,format=yuva444p,colorchannelmixer=aa=0.4[img4];[0:v][img1]overlay=x=5:y=5[01];[01][img2]overlay=x=400:y=5[012];[012][img3]overlay=x=5:y=200[0123];[0123][img4]overlay=x=400:y=200" /mnt/f/video/out.mp4;
 // 简单filtergraphs 视频和音频分别-vf和-af
+
 func main() {
 	// 文字水印
 	go watermark.TextWM()
